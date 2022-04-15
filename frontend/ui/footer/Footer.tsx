160c4359f36855e9640e09f44770b990895df3ed
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-16 00:12:51 +0700

    :sparkles: populate Navbar and Footer

--- frontend/ui/footer/Footer.tsx
+++ frontend/ui/footer/Footer.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "twin.macro";
+import Link from "next/link";
+
+const footerLists = [
+  {
+    name: "About",
+    url: "#about",
+  },
+
+  {
+    name: "Team",
+    url: "#team",
+  },
+];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer tw="bg-primary-dark">
+      <div tw="flex justify-around">
+        <Link href="/">
+          <a>
+            <p tw="text-xl font-bold">AEOLUS</p>
+          </a>
+        </Link>
+        {footerLists.map((footerList) => {
+          return <div>{footerList.name}</div>;
+        })}
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- frontend/ui/navbar/Navbar.tsx
+++ frontend/ui/navbar/Navbar.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import "twin.macro";
+import Link from "next/link";
+
+const navLists = [
+  {
+    name: "About",
+    url: "#about",
+  },
+  {
+    name: "Mechanics",
+    url: "#mechanics",
+  },
+  {
+    name: "Team",
+    url: "#team",
+  },
+];
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav tw="bg-primary-dark">
+      <div tw="flex justify-around">
+        <Link href="/">
+          <a>
+            <p tw="text-xl font-bold">AEOLUS</p>
+          </a>
+        </Link>
+        {navLists.map((navList) => {
+          return <div>{navList.name}</div>;
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
