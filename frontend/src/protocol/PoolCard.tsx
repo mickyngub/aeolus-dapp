e03c9e7f385936fa3385befa6466138a26129a21
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-16 01:05:04 +0700

    :sparkles: create PoolCard

---
+++ frontend/src/protocol/PoolCard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "twin.macro";
+
+const PoolCard = () => {
+  return (
+    <div tw="border-2 border-white w-56">
+      BTC / BNB / SOL
+      <div>
+        <p>Ratio 0.333 : 0.333 : 0.333</p>
+      </div>
+    </div>
+  );
+};
+
+export default PoolCard;
