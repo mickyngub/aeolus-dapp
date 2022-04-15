d238451aed466a5448151cec74f8ef4137707a36
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-15 18:14:23 +0700

    :art: configure absolute import

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
-import GlobalStyles from "../styles/GlobalStyles";
+// import GlobalStyles from "../styles/GlobalStyles";
+import GlobalStyles from "~/styles/GlobalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
