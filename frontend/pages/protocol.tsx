d89067883dbde08ff3fe422c7b7ffb1181f50f37
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-16 01:05:37 +0700

    :lipstick: add dummy layout for Protocol

--- frontend/pages/protocol.tsx
+++ frontend/pages/protocol.tsx
@@ -1,8 +1,24 @@
 import type { ReactElement } from "react";
+import "twin.macro";
+import Dashboard from "~/src/protocol/Dashboard";
+import PoolCard from "~/src/protocol/PoolCard";
+import PriceChart from "~/src/protocol/PriceChart";
 import Layout from "~/src/ui/layout/Layout";
 
 const Protocol = () => {
-  return <div>Protocol</div>;
+  return (
+    <div tw="bg-primary">
+      <div tw="p-6">
+        <Dashboard />
+      </div>
+      <div tw="p-6">
+        <PriceChart />
+      </div>
+      <div tw="p-6">
+        <PoolCard />
+      </div>
+    </div>
+  );
 };
 
 Protocol.getLayout = function getLayout(page: ReactElement) {
