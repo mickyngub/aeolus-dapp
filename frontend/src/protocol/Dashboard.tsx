b7988757552612db38f9964bdf61404c1196ff7b
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-16 01:04:56 +0700

    :sparkles: create Dashboard

---
+++ frontend/src/protocol/Dashboard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "twin.macro";
+
+const Dashboard = () => {
+  return (
+    <div tw="border-2 border-accent">
+      <p>My position</p>
+      <div tw="grid grid-cols-3">
+        <div>Total Supply</div>
+        <div>Total Borrow</div>
+        <div>Total Position</div>
+        <div>Current Price</div>
+        <div>Estimate APR</div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
