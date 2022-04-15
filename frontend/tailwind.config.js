0acc6d55ff37bd644db18ab4afc2a8c83503791b
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-15 21:42:45 +0700

    :art: add tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(28deg 32% 90%)",
+        "primary-dark": "hsl(28deg 32% 80%)",
+        secondary: "hsl(41deg 58% 59%)",
+        accent: "hsl(18deg 75% 55%)",
+        white: "hsl(0deg 0% 100%)",
+        black: "hsl(0deg 0% 0%)",
+      },
+      backgroundImage: {
+        "landing-backround":
+          "linear-gradient(0deg,rgba(15, 23, 42, 1) 0%,rgba(55, 18, 135, 1) 54%,rgba(15, 23, 42, 1) 100% )",
+      },
+    },
+  },
+};
