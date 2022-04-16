f18852f8db88ee6182e43672b6d9316a89fbd6d5
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-17 00:15:47 +0700

    :sparkles: fetcher / get data from API / SSG + CSR for Protocol

--- frontend/pages/protocol/index.tsx
+++ frontend/pages/protocol/index.tsx
@@ -7,14 +7,29 @@ import Dashboard from "~/src/protocol/Dashboard";
 import CryptoCards from "~/src/protocol/CryptoCards/CryptoCards";
 import Button from "~/src/ui/button/Button";
 import Layout from "~/src/ui/layout/Layout";
-import Link from "next/link";
 import PoolCards from "~/src/protocol/PoolCards/PoolCards";
+import { fetcher } from "../api/hello";
+import { SWRConfig } from "swr";
 
 const poolCards = [
   { name: "Bitcoin-USDT", url: "BTC-USDT" },
   { name: "Ethereum-BNB-USDT", url: "ETH-BNB-USDT" },
 ];
-const Protocol = () => {
+
+const coinGeckoAPI = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`;
+
+export async function getStaticProps() {
+  const cryptoData = await fetcher(coinGeckoAPI);
+  return {
+    props: {
+      fallback: {
+        [coinGeckoAPI]: cryptoData,
+      },
+    },
+  };
+}
+
+const Protocol = ({ fallback }: { [key: string]: any }) => {
   const [address, setAddress] = useState<string>("");
   const [signer, setSigner] = useState<ethers.providers.JsonRpcSigner>();
 
@@ -32,29 +47,31 @@ const Protocol = () => {
     toast("Wallet Connected");
   };
   return (
-    <div tw="bg-primary bg-noise">
-      <div tw="p-6 text-right">
-        {!address ? (
-          <Button size="small" onClick={handleConnectWallet}>
-            Connect Wallet
-          </Button>
-        ) : (
-          address
-        )}
+    <SWRConfig value={{ fallback }}>
+      <div tw="bg-primary bg-noise">
+        <div tw="p-6 text-right">
+          {!address ? (
+            <Button size="small" onClick={handleConnectWallet}>
+              Connect Wallet
+            </Button>
+          ) : (
+            address
+          )}
+        </div>
+        <div id="#dashboard" tw="p-6">
+          <Dashboard />
+        </div>
+        <div tw="p-6">
+          <p tw="text-2xl">Cryptocurrency Market</p>
+          <CryptoCards />
+        </div>
+        <div tw="p-6">
+          <p tw="text-2xl ">Pools</p>
+          <PoolCards />
+        </div>
+        <ToastContainer />
       </div>
-      <div id="#dashboard" tw="p-6">
-        <Dashboard />
-      </div>
-      <div tw="p-6">
-        <p tw="text-2xl">Cryptocurrency Market</p>
-        <CryptoCards />
-      </div>
-      <div tw="p-6">
-        <p tw="text-2xl ">Pools</p>
-        <PoolCards />
-      </div>
-      <ToastContainer />
-    </div>
+    </SWRConfig>
   );
 };
 
--- frontend/src/protocol/CryptoCards/CryptoCards.tsx
+++ frontend/src/protocol/CryptoCards/CryptoCards.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import CryptoCard from "./CryptoCard/CryptoCard";
 import "twin.macro";
+import useSWR from "swr";
+import { fetcher } from "~/pages/api/hello";
 
 const cryptoCards = [
   {
@@ -10,10 +12,13 @@ const cryptoCards = [
   { name: "Ethereum", url: "ethereum" },
   { name: "Solana", url: "solana" },
 ];
+const coinGeckoAPI = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`;
 
 const CryptoCards = () => {
+  const { data: cryptoData, error } = useSWR(coinGeckoAPI, fetcher);
   return (
     <div id="#cryptoMarket" tw="flex gap-4">
+      {cryptoData && cryptoData[0].id}
       {cryptoCards
         ? cryptoCards.map((cryptoCard) => {
             return (
