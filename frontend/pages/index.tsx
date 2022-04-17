abf7c346bdb96a3c86a0b63c58a5b8ba61203844
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-17 20:41:22 +0700

    :sparkles: add fallback for Suspense in index and Main

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,53 +1,61 @@
+import React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import tw from "twin.macro";
+import "twin.macro";
 import styled from "styled-components";
 import CanvasWind from "~/src/ui/canvasWind/CanvasWind";
 import Button from "~/src/ui/button/Button";
 import Link from "next/link";
+import { Suspense } from "react";
+import Loading from "~/src/ui/loading/Loading";
 
 const Home: NextPage = () => {
   return (
-    <div tw="h-full bg-primary bg-noise py-6">
+    <div tw="min-h-full bg-primary bg-noise py-6">
       <Head>
         <title>Aeolus</title>
         <meta name="description" content="Aeolus dapp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main tw="">
-        <p tw="text-xl font-bold px-8">
-          The best yield optimizer on AVAX chain
-        </p>
-        <WrapperCanvas>
-          <CanvasWind lightIntensity={0} />
-        </WrapperCanvas>
-        <div tw="text-center my-8">
-          <h1 tw="font-bold text-8xl ">AEOLUS PROTOCOL</h1>
-          <p tw="px-4 mt-4">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus,
-            asperiores possimus in, ratione accusantium quisquam exercitationem
-            laborum libero qui molestias ut fugiat reiciendis cumque dolore sunt
-            rerum. Perferendis, quisquam voluptate?
+      <Suspense
+        fallback={
+          <div tw="h-32 w-32 absolute top-0 bottom-0 right-0 left-0 m-auto grid place-content-center  ">
+            <Loading />
+          </div>
+        }
+      >
+        <main tw="">
+          <p tw="text-xl font-bold px-8">
+            The best yield optimizer on AVAX chain
           </p>
-        </div>
-
-        <div tw="flex justify-center my-8">
-          <Link href="/main">
-            <a>
-              <Button size="medium">Get Started</Button>
-            </a>
-          </Link>
-        </div>
-
-        <WrapperCanvas>
-          <CanvasWind lightIntensity={0} />
-        </WrapperCanvas>
-        <p tw="text-xl font-bold px-8 text-right">
-          Optimize your investment in one-click
-        </p>
-      </main>
+          <WrapperCanvas>
+            <CanvasWind lightIntensity={0} />
+          </WrapperCanvas>
+          <div tw="text-center my-8">
+            <h1 tw="font-bold text-8xl ">AEOLUS PROTOCOL</h1>
+            <p tw="px-4 mt-4">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Doloribus, asperiores possimus in, ratione accusantium quisquam
+              exercitationem laborum libero qui molestias ut fugiat reiciendis
+              cumque dolore sunt rerum. Perferendis, quisquam voluptate?
+            </p>
+          </div>
+          <div tw="flex justify-center my-8">
+            <Link href="/main">
+              <a>
+                <Button size="medium">Get Started</Button>
+              </a>
+            </Link>
+          </div>
+          <WrapperCanvas>
+            <CanvasWind lightIntensity={0} />
+          </WrapperCanvas>
+          <p tw="text-xl font-bold px-8 text-right">
+            Optimize your investment in one-click
+          </p>
+        </main>
+      </Suspense>
     </div>
   );
 };
--- frontend/pages/main/index.tsx
+++ frontend/pages/main/index.tsx
@@ -1,75 +1,86 @@
 import "twin.macro";
 import type { ReactElement } from "react";
+import { Suspense } from "react";
 import Button from "~/src/ui/button/Button";
 import Layout from "~/src/ui/layout/Layout";
 import Link from "next/link";
 import CanvasWind from "~/src/ui/canvasWind/CanvasWind";
+import Loading from "~/src/ui/loading/Loading";
 
 const Main = () => {
   return (
-    <main tw="bg-primary bg-noise">
-      <div tw="relative">
-        <CanvasWind lightIntensity={0.3} />
-        <div tw="absolute bottom-0 px-8 pb-2">
-          <p tw="text-7xl text-center text-white">AEOLUS PROTOCOL</p>
+    <Suspense
+      fallback={
+        <div tw="min-h-full bg-primary bg-noise">
+          <div tw="h-32 w-32 absolute top-0 bottom-0 right-0 left-0 m-auto grid place-content-center  ">
+            <Loading />
+          </div>
         </div>
-      </div>
-      <div tw="grid grid-cols-2 gap-8 p-8">
-        <div id="#about" tw="">
-          <p tw="text-2xl">{"What's Aeolus Protocol?"}</p>
+      }
+    >
+      <main tw="bg-primary bg-noise">
+        <div tw="relative">
+          <CanvasWind lightIntensity={0.3} />
+          <div tw="absolute bottom-0 px-8 pb-2">
+            <p tw="text-7xl text-center text-white">AEOLUS PROTOCOL</p>
+          </div>
         </div>
-        <div tw="">
-          <p tw="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-            sapiente eligendi nisi iste quisquam adipisci nemo voluptate,
-            labore, accusantium tenetur dolor officiis pariatur accusamus
-            consequatur consequuntur dicta, culpa et velit. Atque delectus dolor
-            laudantium sequi quae beatae. Aliquam, corporis error asperiores
-            repellendus explicabo sint dolore tempora pariatur iure eius hic ab
-            provident obcaecati id, ipsum, nesciunt maxime enim. Voluptates,
-            possimus!
-          </p>
+        <div tw="grid grid-cols-2 gap-8 p-8">
+          <div id="#about" tw="">
+            <p tw="text-2xl">{"What's Aeolus Protocol?"}</p>
+          </div>
+          <div tw="">
+            <p tw="text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              sapiente eligendi nisi iste quisquam adipisci nemo voluptate,
+              labore, accusantium tenetur dolor officiis pariatur accusamus
+              consequatur consequuntur dicta, culpa et velit. Atque delectus
+              dolor laudantium sequi quae beatae. Aliquam, corporis error
+              asperiores repellendus explicabo sint dolore tempora pariatur iure
+              eius hic ab provident obcaecati id, ipsum, nesciunt maxime enim.
+              Voluptates, possimus!
+            </p>
+          </div>
+          <div tw="">
+            <p tw="text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              sapiente eligendi nisi iste quisquam adipisci nemo voluptate,
+              labore, accusantium tenetur dolor officiis pariatur accusamus
+              consequatur consequuntur dicta, culpa et velit. Atque delectus
+              dolor laudantium sequi quae beatae. Aliquam, corporis error
+              asperiores repellendus explicabo sint dolore tempora pariatur iure
+              eius hic ab provident obcaecati id, ipsum, nesciunt maxime enim.
+              Voluptates, possimus!
+            </p>
+          </div>
+          <div id="#mechanics" tw="">
+            <p tw="text-2xl">How does Aeolus Protocol work?</p>
+          </div>
+          <div id="#team" tw="">
+            <p tw="text-2xl">{"Who's the team behind Aeolus Protocol?"}</p>
+          </div>
+          <div tw="">
+            <p tw="text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
+              sapiente eligendi nisi iste quisquam adipisci nemo voluptate,
+              labore, accusantium tenetur dolor officiis pariatur accusamus
+              consequatur consequuntur dicta, culpa et velit. Atque delectus
+              dolor laudantium sequi quae beatae. Aliquam, corporis error
+              asperiores repellendus explicabo sint dolore tempora pariatur iure
+              eius hic ab provident obcaecati id, ipsum, nesciunt maxime enim.
+              Voluptates, possimus!
+            </p>
+          </div>
         </div>
-
-        <div tw="">
-          <p tw="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-            sapiente eligendi nisi iste quisquam adipisci nemo voluptate,
-            labore, accusantium tenetur dolor officiis pariatur accusamus
-            consequatur consequuntur dicta, culpa et velit. Atque delectus dolor
-            laudantium sequi quae beatae. Aliquam, corporis error asperiores
-            repellendus explicabo sint dolore tempora pariatur iure eius hic ab
-            provident obcaecati id, ipsum, nesciunt maxime enim. Voluptates,
-            possimus!
-          </p>
+        <div tw="grid place-items-center py-12">
+          <Link href="/protocol">
+            <a>
+              <Button size="medium">Enter App</Button>
+            </a>
+          </Link>
         </div>
-        <div id="#mechanics" tw="">
-          <p tw="text-2xl">How does Aeolus Protocol work?</p>
-        </div>
-        <div id="#team" tw="">
-          <p tw="text-2xl">{"Who's the team behind Aeolus Protocol?"}</p>
-        </div>
-        <div tw="">
-          <p tw="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-            sapiente eligendi nisi iste quisquam adipisci nemo voluptate,
-            labore, accusantium tenetur dolor officiis pariatur accusamus
-            consequatur consequuntur dicta, culpa et velit. Atque delectus dolor
-            laudantium sequi quae beatae. Aliquam, corporis error asperiores
-            repellendus explicabo sint dolore tempora pariatur iure eius hic ab
-            provident obcaecati id, ipsum, nesciunt maxime enim. Voluptates,
-            possimus!
-          </p>
-        </div>
-      </div>
-      <div tw="grid place-items-center py-12">
-        <Link href="/protocol">
-          <a>
-            <Button size="medium">Enter App</Button>
-          </a>
-        </Link>
-      </div>
-    </main>
+      </main>
+    </Suspense>
   );
 };
 
