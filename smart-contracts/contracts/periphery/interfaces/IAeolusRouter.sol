a63e56c3072c54eda091fc63dd9b9fb17f21b3bb
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-21 23:11:46 +0700

    :construction: createIAeolusRouter

---
+++ smart-contracts/contracts/periphery/interfaces/IAeolusRouter.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT LICENSE
+pragma solidity ^0.8.9;
+
+interface IAeolusRouter {
+    function factory() external pure returns (address);
+
+    function investPair(
+        address usdt,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
+    function investTriple(
+        address usdt,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
+    function redeemPair(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+
+    function redeemTriple(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+}
