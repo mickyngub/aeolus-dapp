5eaa1bfddf57c9826279dad3171b13f0301f0316
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-21 16:45:01 +0700

    :fire: remove unused code

--- smart-contracts/contracts/interfaces/IAeolusERC20.sol
+++ smart-contracts/contracts/interfaces/IAeolusERC20.sol
@@ -2,9 +2,6 @@
 pragma solidity ^0.8.9;
 
 interface IAeolusERC20 {
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
     function name() external pure returns (string memory);
 
     function symbol() external pure returns (string memory);
@@ -26,20 +23,4 @@ interface IAeolusERC20 {
         address to,
         uint256 value
     ) external returns (bool);
-
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-
-    function PERMIT_TYPEHASH() external pure returns (bytes32);
-
-    function nonces(address owner) external view returns (uint256);
-
-    function permit(
-        address owner,
-        address spender,
-        uint256 value,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external;
 }
