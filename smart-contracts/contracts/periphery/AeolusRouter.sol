e9cdfa64accab8cab605a29b79cd0536e85eb57e
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-23 22:37:20 +0700

    :construction: add WAVAX / swap function

--- smart-contracts/contracts/periphery/AeolusRouter.sol
+++ smart-contracts/contracts/periphery/AeolusRouter.sol
@@ -9,18 +9,19 @@ import "./interfaces/IAeolusRouter.sol";
 import "./interfaces/IExchangeRouter.sol";
 
 import "../core/interfaces/IERC20.sol";
-import "../core/interfaces/IAeolusFactory.sol";
+import "../core/AeolusFactory.sol";
 import "../core/interfaces/IAeolusPair.sol";
 
 contract AeolusRouter is IAeolusRouter, Ownable {
-    IAeolusFactory public FACTORY;
+    AeolusFactory public FACTORY;
     // Exchange Router for swapping, addding lp, removing lp
     IExchangeRouter public ROUTER;
 
     address public constant USDTdotE = 0xc7198437980c041c805a1edcba50c1ce5db95118;
+    address public constant WAVAX = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
 
     constructor(address _factory, address _router) {
-        FACTORY = IAeolusFactory(_factory);
+        FACTORY = AeolusFactory(_factory);
         ROUTER = IExchangeRouter(_router);
     }
 
@@ -36,6 +37,10 @@ contract AeolusRouter is IAeolusRouter, Ownable {
     {
         IERC20(USDTdotE).transferFrom(msg.sender, address(this), amount);
         _approveTokenIfNeeded(USDTdotE);
+
+        (, address tokenA, address tokenB) = FACTORY.getPair(pairID);
+        address tokenAStable = FACTORY.getStableAddressOfApprovedToken(tokenA);
+        address tokenBStable = FACTORY.getStableAddressOfApprovedToken(tokenB);
     }
 
     // **** REMOVE LIQUIDITY ****
@@ -48,23 +53,38 @@ contract AeolusRouter is IAeolusRouter, Ownable {
         address to
     ) public returns (uint256 amountA, uint256 amountB) {}
 
-    /** 
-    ADMIN Function for updating FACTORY and ROUTER
-     */
-
-    function updateFactory(address _factory) external onlyOwner {
-        FACTORY = IAeolusFactory(_factory);
-    }
-
-    function updateExchangeRouter(address _router) external onlyOwner {
-        ROUTER = IExchangeRouter(_router);
-    }
-
     /* ========== Private Functions ========== */
 
     function _approveTokenIfNeeded(address token) private {
         if (IERC20(token).allowance(address(this), address(ROUTER)) == 0) {
             IERC20(token).approve(address(ROUTER), type(uint256).max);
         }
+    }
+
+    function _swap(
+        address _from,
+        uint256 amount,
+        address _to,
+        address receiver
+    ) private returns (uint256) {
+        address[] memory path;
+
+        path = new address[](3);
+        path[0] = _from;
+        path[1] = WAVAX;
+        path[2] = _to;
+
+        uint256[] memory amounts = ROUTER.swapExactTokensForTokens(amount, 0, path, receiver, block.timestamp);
+        return amounts[amounts.length - 1];
+    }
+
+    /* ========== Admin FUNCTIONS ========== */
+
+    function updateFactory(address _factory) external onlyOwner {
+        FACTORY = AeolusFactory(_factory);
+    }
+
+    function updateExchangeRouter(address _router) external onlyOwner {
+        ROUTER = IExchangeRouter(_router);
     }
 }
