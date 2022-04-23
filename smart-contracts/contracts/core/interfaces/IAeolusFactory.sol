4e9de61f5c2c3c1110915c703fb240e07de4ff27
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-04-23 17:47:53 +0700

    :construction: temporarily remove createTriple

--- smart-contracts/contracts/core/interfaces/IAeolusFactory.sol
+++ smart-contracts/contracts/core/interfaces/IAeolusFactory.sol
@@ -2,15 +2,13 @@
 pragma solidity ^0.8.9;
 
 interface IAeolusFactory {
-    function getApprovedTokenStablePair(address token) external view returns (address tokenStable);
-
     function getNumberOfPools() external view returns (uint256);
 
-    function createPair(string memory _tokenSymbolA, string memory _tokenSymbolB) external returns (address pair);
+    function createPair(string memory _tokenSymbolA, string memory _tokenSymbolB) external returns (string memory pairName);
 
-    function createTriple(
-        address tokenA,
-        address tokenB,
-        address tokenC
-    ) external returns (address triple);
+    // function createTriple(
+    //     address tokenA,
+    //     address tokenB,
+    //     address tokenC
+    // ) external returns (address triple);
 }
