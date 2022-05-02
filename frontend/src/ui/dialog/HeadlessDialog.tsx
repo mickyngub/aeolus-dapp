88118c9cd128414355026796d8daacc1d463bed7
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-05-02 22:58:27 +0700

    :sparkles: create HeadlessDialog

---
+++ frontend/src/ui/dialog/HeadlessDialog.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import "twin.macro";
+
+const HeadlessModal = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Dialog open={isOpen} onClose={() => setIsOpen(false)} tw="bg-blue-600">
+      <Dialog.Panel>
+        <Dialog.Title tw="bg-blue-600">Logout</Dialog.Title>
+        <Dialog.Description>
+          This will log you out of Aeolus Protocol
+        </Dialog.Description>
+
+        <button onClick={() => setIsOpen(false)}>Logout</button>
+        <button onClick={() => {}}>Cancel</button>
+      </Dialog.Panel>
+    </Dialog>
+  );
+};
+
+export default HeadlessModal;
