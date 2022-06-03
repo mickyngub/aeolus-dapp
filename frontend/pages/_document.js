0cdfaaaa2481a6f40f80cd906394d6b46f158a95
Pichaya Puttekulangkura <micky47561@gmail.com>
2022-06-03 01:50:52 -0400

    :sparkles: add default document title

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -40,6 +40,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&display=swap"
             rel="stylesheet"
           />
+          <title>Aeolus Protocol</title>
         </Head>
         <body>
           <Main />
